68e855ad0df7a3c437103e91417ac8a9ea718d53
neoan <stefan.roehrl@gmail.com>
2022-08-01 20:36:04 -0400

    Initial commit

---
+++ DatabaseAdaptor.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace NeoanIo\MarketPlace;
+
+use Exception;
+use Neoan\Database\Adaptor;
+use Neoan3\Apps\DbException;
+use Neoan3\Apps\DbOOP;
+
+class DatabaseAdaptor extends DbOOP implements Adaptor
+{
+
+    /**
+     * @throws Exception
+     */
+    public function raw(string $sql, array $conditions, mixed $extra)
+    {
+        try{
+            return $this->smart('>' . $sql, $conditions, $extra);
+        } catch (DbException $e){
+            throw new Exception($e->getMessage());
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function insert($table, array $content)
+    {
+        try{
+            $this->sanitizePassword($content);
+            return $this->smart($table, $content);
+        } catch (DbException $e){
+            throw new Exception($e->getMessage());
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function update($table, array $values, array $where)
+    {
+        try{
+            $this->sanitizePassword($values);
+            return $this->smart($table, $values, $where);
+        } catch (DbException $e){
+            throw new Exception($e->getMessage());
+        }
+    }
+
+    public function delete($table, string $id, bool $hard = false)
+    {
+        try{
+            return $this->delete($table, $id, $hard);
+        } catch (DbException $e){
+            throw new Exception($e->getMessage());
+        }
+    }
+    private function sanitizePassword(array &$conditions): void
+    {
+        if(isset($conditions['password']) && str_starts_with($conditions['password'],'$')){
+            $conditions['password'] = '=' . $conditions['password'];
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Easy database adaptor for neoanIo and neoan3-apps/db
+
+This wrapper enables the use of the battle-tested neoan3 
+legacy default database handler [neoan3-apps/db](https://packagist.org/packages/neoan3-apps/db) 
